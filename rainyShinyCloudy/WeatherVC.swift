64732c025f1c540f5ded613cc5569998d907f199
TaimoorSaeed <taimoor.dev@gmail.com>
2017-06-29 08:08:31 +0500

    fixes

--- rainyShinyCloudy/WeatherVC.swift
+++ rainyShinyCloudy/WeatherVC.swift
@@ -12,7 +12,6 @@ import UIKit
 class WeatherVC: UIViewController {
 
     
-    //labels
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var currentTemoLabel: UILabel!
