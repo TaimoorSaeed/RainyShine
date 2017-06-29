87af460a255af701582c7ba12ef9f83d5781328b
TaimoorSaeed <taimoor.dev@gmail.com>
2017-06-30 00:03:59 +0500

    data model and constants added

---
+++ rainyShinyCloudy/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  rainyShinyCloudy
+//
+//  Created by Taimoor Saeed on 29/06/2017.
+//  Copyright © 2017 Taimoor. All rights reserved.
+//
+
+import Foundation
+
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let API_ID = "&appid="
+let API_KEY = "7d93fa1fca5b9d823a05232e06a6df72"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(API_ID)\(API_KEY)"
+
+
+
