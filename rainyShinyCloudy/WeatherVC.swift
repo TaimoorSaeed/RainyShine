1bc0fee04c8f6e54bd55b3891299668273d4f317
TaimoorSaeed <taimoor.dev@gmail.com>
2017-06-29 08:00:01 +0500

    fixes

---
+++ rainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,32 @@
+
+//
+//  WeatherVC.swift
+//  rainyShinyCloudy
+//
+//  Created by Taimoor Saeed on 29/06/2017.
+//  Copyright © 2017 Taimoor. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    
+    //labels
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentTemoLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+
+}
+
