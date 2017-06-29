04f472358aff10575324b99e6e0ab25107bcc3a9
TaimoorSaeed <taimoor.dev@gmail.com>
2017-06-29 08:30:24 +0500

    tableView added and tested

--- rainyShinyCloudy/WeatherVC.swift
+++ rainyShinyCloudy/WeatherVC.swift
@@ -9,8 +9,8 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
-
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var locationLabel: UILabel!
@@ -21,11 +21,26 @@ class WeatherVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-   
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+    
+    
 }
 
